7667a2cad15fa8ad9bf9de66095b9cae578ba1fc
scottblender <57569396+scottblender@users.noreply.github.com>
2020-06-04 15:31:00 -0400

    Update InfoHolder.js

--- src/InfoHolder.js
+++ src/InfoHolder.js
@@ -5,6 +5,7 @@ import useLockBodyScroll from './useLockBodyScroll'
 import Aux from './Auxilliary'
 const InfoHolder = () => {
     const [isHovering, setHovering] = useState(false);
+    const [isHovering2, setHovering2] = useState(false);;;
     useLockBodyScroll()
     return(
         <Aux>
@@ -13,6 +14,11 @@ const InfoHolder = () => {
         <img onMouseEnter={() => setHovering(!isHovering)} onMouseLeave={() => setHovering(!isHovering)}src={Chinmayi} alt=''>
         </img>
         {isHovering ?  <p>My name is Chinmayi</p> : ''}
+        </div>
+        <div>
+        <img onMouseEnter={() => setHovering2(!isHovering2)} onMouseLeave={() => setHovering2(!isHovering2)}src={Chinmayi} alt=''>
+        </img>
+        {isHovering2 ?  <p>My name is Chinmayi</p> : ''}
         </div>
         </div>
         </Aux>
