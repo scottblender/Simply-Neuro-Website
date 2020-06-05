94bceed961ad111170d454bee297ab4e7ec74f38
scottblender <57569396+scottblender@users.noreply.github.com>
2020-06-05 07:58:14 -0400

    Typeform

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Aux from './Auxilliary';
 import useLockBodyScroll from './useLockBodyScroll'
 import Content from './Content'
+import logo from './assets/images/ICON.png'
 
 function App() {
   useLockBodyScroll()
   return (
     <Aux>
     <div className="App">
+    <img className='image'src={logo} alt=''></img>
   <Content></Content>
     </div>
     
--- src/Content.js
+++ src/Content.js
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Content.css'
 import Aux from './Auxilliary'
-import logo from './assets/images/ICON.png'
-const Content = () => {
+import  {ReactTypeformEmbed}  from 'react-typeform-embed'
+class Content extends Component {
+  constructor(props) {
+    super(props)
+    this.openFormHandeler = this.openFormHandeler.bind(this)
+}
+
+// Handler to open the form
+openFormHandeler() {
+    this.typeformEmbed.typeform.open();
+}
+  render(){
     return(
         <Aux>
-    <div className='Container3'>
-    <img className='image'src={logo} alt=''></img>
+  
     <div className='text'>
-    <div className="title">
     <h1>Simply Neuroscience</h1>
-    </div>
-    <div className="paragraph">
     <p>Pursuing the brain, Pursuing the brain, 
       unlocking the future, 
       one neuron at a time. 
       </p>
-    </div>
-    <div className='learn'>
-      <button>Learn more...</button>
-    </div>
-    </div>
+      <ReactTypeformEmbed
+          popup
+          autoOpen={false}
+          url="https://chinmayibalusu.typeform.com/to/y8ouTz"
+          hideHeaders
+          hideFooter
+          buttonText="Go!"
+          style={{ top: 100 }}
+          ref={tf => {
+            this.typeformEmbed = tf;
+          }}
+        />
+      <button onClick={this.openFormHandeler}>Learn more...</button>
     </div>
     </Aux>
     )
+}
 }
 
 export default Content;
