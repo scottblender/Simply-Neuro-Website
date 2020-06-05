ee37a8d3ec4f549059240b261d17f7ec160ea781
scottblender <57569396+scottblender@users.noreply.github.com>
2020-06-05 02:41:15 -0400

    Update navigation

--- src/Menu.js
+++ src/Menu.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './Menu.css'
 import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
-const menu = () => {
+export default ({close}) => {
     return(
         <div className ='menu'>
         <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li>About Us</li>
-            <li><Link to='/InfoHolder'>Our Team</Link></li>
-            <li>Partners</li>
-            <li>Blog</li>
+            <li onClick = {close}><Link to='/'>Home</Link></li>
+            <li onClick = {close}>About Us</li>
+            <li onClick = {close}><Link to='/InfoHolder'>Our Team</Link></li>
+            <li onClick = {close}>Partners</li>
+            <li onClick = {close}>Blog</li>
         </ul>
         </div>
     )
 }
-export default menu;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,36 +1,35 @@
 import React, {Component} from 'react'
 import Aux from './Auxilliary'
-import './Navigation.css'
 import Menu from './Menu'
-
+import BurgerIcon from "./BurgerIcon";
+import Popup from "reactjs-popup";
 class Navigation extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            openNav : false,
             displayData : [props.children]
         };
     }
+   contentStyle = {
+        background: "rgba(255,255,255,0",
+        width: "80%",
+        border: "none"
+      };
     render(){
-        const openNavigation = this.state.openNav
-        let navItems;
         let onScreenDisplay = this.state.displayData
-        if(openNavigation){
-            navItems =  
-            <Menu></Menu> 
-            onScreenDisplay = <div></div>
-        }
         return(
             <Aux>
-            <span onClick={() => this.setState({openNav:!this.state.openNav})}>
-            <div className={openNavigation ? "burger-menu open" : "burger-menu"}>
-            <div className="bar1" key="b1" />
-            <div className="bar2" key="b2" />
-            <div className="bar3" key="b3" />
-            </div>
-            </span>
-             {navItems} 
+            <Popup
+                modal
+                overlayStyle={{ background: "rgba(12, 92, 99, 0.85)" }}
+                contentStyle={this.contentStyle}
+                closeOnDocumentClick = {false}
+                trigger = {open => <BurgerIcon open={open}></BurgerIcon>}
+            >
+             {close => <Menu close={close} />}
+            </Popup>
+           
              {onScreenDisplay}
                </Aux>
         )
