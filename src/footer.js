9a283c05f3bd65e2ac074812f8858740c5ff775d
Jinhao <53026474+jinhao27@users.noreply.github.com>
2020-06-07 16:01:16 -0400

    added links to socials

--- src/footer.js
+++ src/footer.js
@@ -7,9 +7,9 @@ const footer = () => {
     return(
         <div className='Container1'>
             <h3>Follow us on our socials!</h3>
-            <img src = {insta} alt = ''></img>
-            <img src = {twitter} alt = ''></img>
-            <img src = {medium} alt = ''></img>
+            <a href="https://www.instagram.com/simplyneuroscience/" target="_blank"><img src = {insta} alt = ''></img></a>
+            <a href="https://twitter.com/simplyneurosci?lang=en" target="_blank"><img src = {twitter} alt = ''></img></a>
+            <a href="https://medium.com/simply-neuroscience" target="_blank"><img src = {medium} alt = ''></img></a>
         </div>
     )
 }
